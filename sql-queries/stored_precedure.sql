80b5e335386a3da5ff542243d9dab710bc1d3dba
alligm79 <aligolmohamadi1379@gmailcom>
2025-03-22 17:35:02 +0330

    functions 1-3

--- sql-queries/stored_precedure.sql
+++ sql-queries/stored_precedure.sql
@@ -1,4 +1,68 @@
 -----------------------------------------------------------
+--- 1. گرفتن کل بلیط های یک کاربر با استفاده از ایمیل او
+-----------------------------------------------------------
+CREATE OR REPLACE FUNCTION get_tickets_of_users(user_email varchar(100))
+RETURNS TABLE (TicketID UUID,
+    VehicleType VARCHAR(20),
+    Origin VARCHAR(100),
+    Destination VARCHAR(100),
+    DepartureTime TIMESTAMP,
+    ArrivalTime TIMESTAMP,
+    TicketPrice DECIMAL(10,2),
+    RemainingCapacity INT,
+    CompanyID UUID,
+    TravelClass VARCHAR(20)) AS 
+$$
+BEGIN
+	RETURN QUERY SELECT t.* FROM ticket t JOIN reservation r ON t.ticketid = r.ticketid
+			   WHERE r.userid IN (SELECT userid FROM "User" WHERE email = user_email);
+END;
+$$ 
+LANGUAGE plpgsql;
+
+-----------------------------------------------------------
+--- 2. نشان دادن نام کاربرانی که حداقل یک بار بلیط ان ها کنسل شده با استفاده از ایمیل پشتیبان
+-----------------------------------------------------------
+CREATE OR REPLACE FUNCTION get_users_with_cancelled_reservation_using_support_email(support_email varchar(100))
+RETURNS TABLE (
+	firstname varchar(50),
+  lastname varchar(50)) AS 
+$$
+BEGIN
+	IF support_email NOT IN (SELECT email FROM "User" WHERE usertype = 'Supporter') THEN
+		RAISE 'SUPPORTER NOT FOUND';
+	END IF;
+	RETURN QUERY SELECT u.firstname, u.lastname FROM "User" u JOIN reservation r 
+		ON u.userid = r.userid WHERE r.reservationstatus = 'Cancelled';
+END;
+$$ 
+LANGUAGE plpgsql;
+
+-----------------------------------------------------------
+--- 3. لیست بلیط های خریداری شده یک شهر با دریافت نام آن.
+-----------------------------------------------------------
+CREATE OR REPLACE FUNCTION get_bought_tickets_of_a_city(city varchar(100))
+RETURNS TABLE (TicketID UUID,
+    VehicleType VARCHAR(20),
+    Origin VARCHAR(100),
+    Destination VARCHAR(100),
+    DepartureTime TIMESTAMP,
+    ArrivalTime TIMESTAMP,
+    TicketPrice DECIMAL(10,2),
+    RemainingCapacity INT,
+    CompanyID UUID,
+    TravelClass VARCHAR(20)) AS 
+$$
+BEGIN 
+	RETURN QUERY SELECT t.* FROM ticket t 
+		JOIN reservation r ON t.ticketid = r.ticketid
+		JOIN payment p ON p.reservationid = r.reservationid
+		WHERE p.paymentstatus = 'Successful' AND t.destination = city;
+END;
+$$
+LANGUAGE plpgsql;
+
+-----------------------------------------------------------
 -- عبارتی را از ورودی گرفته و بلیطهایی را که آن عبارت در نام مسافر، مسیر سفر یا کلاس بلیط آمده باشد را برگردانید.4
 -----------------------------------------------------------
 
