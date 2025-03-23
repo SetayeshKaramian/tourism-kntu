93df37f83ca7b6826eb85a0c68ad1f4e3d8d0149
alligm79 <aligolmohamadi1379@gmailcom>
2025-03-23 19:13:09 +0330

    FIX: functions to Stored procedures

--- sql-queries/stored_precedure.sql
+++ sql-queries/stored_precedure.sql
@@ -1,66 +1,68 @@
 -----------------------------------------------------------
 --- 1. گرفتن کل بلیط های یک کاربر با استفاده از ایمیل او
 -----------------------------------------------------------
-CREATE OR REPLACE FUNCTION get_tickets_of_users(user_email varchar(100))
-RETURNS TABLE (TicketID UUID,
-    VehicleType VARCHAR(20),
-    Origin VARCHAR(100),
-    Destination VARCHAR(100),
-    DepartureTime TIMESTAMP,
-    ArrivalTime TIMESTAMP,
-    TicketPrice DECIMAL(10,2),
-    RemainingCapacity INT,
-    CompanyID UUID,
-    TravelClass VARCHAR(20)) AS 
-$$
+CREATE OR REPLACE PROCEDURE get_tickets_of_users(
+	IN user_email VARCHAR(100))
+LANGUAGE plpgsql
+AS $$ 
+DECLARE 
+	tickets refcursor;
 BEGIN
-	RETURN QUERY SELECT t.* FROM ticket t JOIN reservation r ON t.ticketid = r.ticketid
+	tickets := 'tickets';
+	
+	OPEN tickets FOR 
+		SELECT t.* FROM ticket t JOIN reservation r ON t.ticketid = r.ticketid
 			   WHERE r.userid IN (SELECT userid FROM "User" WHERE email = user_email);
+			   
 END;
-$$ 
-LANGUAGE plpgsql;
+$$;
+
+CALL get_tickets_of_users('maryam.jafari@example.com');
+FETCH ALL FROM tickets;
 
 -----------------------------------------------------------
 --- 2. نشان دادن نام کاربرانی که حداقل یک بار بلیط ان ها کنسل شده با استفاده از ایمیل پشتیبان
 -----------------------------------------------------------
-CREATE OR REPLACE FUNCTION get_users_with_cancelled_reservation_using_support_email(support_email varchar(100))
-RETURNS TABLE (
-	firstname varchar(50),
-  lastname varchar(50)) AS 
-$$
+CREATE OR REPLACE PROCEDURE get_users_with_cancelled_reservation_using_support_email(
+	IN support_email VARCHAR(100)
+)
+LANGUAGE plpgsql AS $$
+DECLARE
+	users refcursor;
 BEGIN
-	IF support_email NOT IN (SELECT email FROM "User" WHERE usertype = 'Supporter') THEN
-		RAISE 'SUPPORTER NOT FOUND';
-	END IF;
-	RETURN QUERY SELECT u.firstname, u.lastname FROM "User" u JOIN reservation r 
-		ON u.userid = r.userid WHERE r.reservationstatus = 'Cancelled';
+	users := 'users';
+	
+	OPEN users FOR 
+		SELECT u.firstname, u.lastname FROM "User" u JOIN reservation r 
+			ON u.userid = r.userid WHERE r.reservationstatus = 'Cancelled';
 END;
-$$ 
-LANGUAGE plpgsql;
+$$;
+
+CALL get_users_with_cancelled_reservation_using_support_email('ehsan.shirazi@example.com');
+FETCH ALL FROM users;
 
 -----------------------------------------------------------
 --- 3. لیست بلیط های خریداری شده یک شهر با دریافت نام آن.
 -----------------------------------------------------------
-CREATE OR REPLACE FUNCTION get_bought_tickets_of_a_city(city varchar(100))
-RETURNS TABLE (TicketID UUID,
-    VehicleType VARCHAR(20),
-    Origin VARCHAR(100),
-    Destination VARCHAR(100),
-    DepartureTime TIMESTAMP,
-    ArrivalTime TIMESTAMP,
-    TicketPrice DECIMAL(10,2),
-    RemainingCapacity INT,
-    CompanyID UUID,
-    TravelClass VARCHAR(20)) AS 
-$$
+CREATE OR REPLACE PROCEDURE get_bought_tickets_of_a_city(
+	IN city VARCHAR(100)
+)
+LANGUAGE plpgsql AS $$
+DECLARE
+	tickets refcursor;
 BEGIN 
-	RETURN QUERY SELECT t.* FROM ticket t 
+	tickets := 'tickets';
+	
+	OPEN tickets FOR 
+		SELECT t.* FROM ticket t 
 		JOIN reservation r ON t.ticketid = r.ticketid
 		JOIN payment p ON p.reservationid = r.reservationid
 		WHERE p.paymentstatus = 'Successful' AND t.destination = city;
 END;
-$$
-LANGUAGE plpgsql;
+$$;
+
+CALL get_bought_tickets_of_a_city('Mashhad');
+FETCH ALL FROM tickets;
 
 -----------------------------------------------------------
 -- عبارتی را از ورودی گرفته و بلیطهایی را که آن عبارت در نام مسافر، مسیر سفر یا کلاس بلیط آمده باشد را برگردانید.4
